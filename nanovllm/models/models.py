8e8d2581017c85a8b2435d24c40bf4829e39ffc1
dsh <dsh@MacOS.local>
2025-09-01 16:24:16 +0800

    support qwen2

--- nanovllm/models/models.py
+++ nanovllm/models/models.py
@@ -1,10 +1,13 @@
+from .qwen2 import Qwen2ForCausalLM
 from .qwen3 import Qwen3ForCausalLM
 from .qwen3_moe import Qwen3MoeForCausalLM
 from .llama import LlamaForCausalLM
 
 # Register Model
 model_dict = {
+    "qwen2": Qwen2ForCausalLM,
     "qwen3": Qwen3ForCausalLM,
-    "llama": LlamaForCausalLM,
     "qwen3_moe": Qwen3MoeForCausalLM,
+    "llama": LlamaForCausalLM,
 }
+
